284b0865bb892ae69342041c635dfdc54a518950
smart4654154 <402407974@qq.com>
2025-09-02 17:33:14 +0800

    删除多余的

--- examples/assets/download_dragon_mesh.sh
+++
@@ -1,5 +0,0 @@
-set -e -x
-
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-
-gdown "https://drive.google.com/uc?id=1uRDvoS_l2Or8g8YDDPYV79K6_RfFYBeF" -O "${SCRIPT_DIR}"/dragon.obj
--- examples/assets/download_gsplat_ckpt.sh
+++
@@ -1,5 +0,0 @@
-set -e -x
-
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-
-gdown "https://drive.google.com/uc?id=137hJErx-gDd_2kkhKJ4NEM75-DiEtuhF" -O "${SCRIPT_DIR}"/ckpt_6999_crop.pt
